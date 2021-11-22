2b82fc163fc284c51dd18849c6ae379c8de1c1d2
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2021-11-22 22:31:49 +0900

    モザイク一覧を取得

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,15 +1,60 @@
-import './ExploreContainer.css';
+import React, { useState } from "react";
+import "./ExploreContainer.css";
+import { Address, RepositoryFactoryHttp } from "symbol-sdk";
+import { IonButton } from "@ionic/react";
 
-interface ContainerProps {
-  name: string;
-}
+const ExploreContainer = () => {
+  const [mosaicArray, setMosaicArray] = useState<any[]>([]);
+  var loop = () => {
+    const items = [];
+    for (let i = 0; i < mosaicArray.length; i++) {
+      items.push(
+        <li key={mosaicArray[i].id}>
+          <p>{mosaicArray[i].id}</p>
+          <p>{mosaicArray[i].amount}</p>
+        </li>
+      );
+    }
+    return <ul>{items}</ul>;
+  };
 
-const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  const getAccountMosaics = () => {
+    const rawAddress = "TAEFXKFB4GJEO7ZSUN2RG4ZB2GIN4WKLGPQHSPQ";
+    const address = Address.createFromRawAddress(rawAddress);
+    // replace with node endpoint
+    const nodeUrl = "http://ngl-dual-101.testnet.symboldev.network:3000";
+    const repositoryFactory = new RepositoryFactoryHttp(nodeUrl);
+    const accountHttp = repositoryFactory.createAccountRepository();
+    const divisibility = 6;
+    const mosaicsArrayList: any[] = [];
+
+    accountHttp.getAccountInfo(address).subscribe(
+      (accountInfo) => {
+        console.log(accountInfo);
+        for (let i = 0; i < accountInfo.mosaics.length; i++) {
+          const mosaics = { id: "", amount: 0 };
+          mosaics.id = accountInfo.mosaics[i].id.toHex();
+          if (mosaics.id === "091F837E059AE13C") {
+            mosaics.amount =
+              accountInfo.mosaics[i].amount.compact() /
+              Math.pow(10, divisibility);
+          } else {
+            mosaics.amount =
+              accountInfo.mosaics[i].amount.compact() / Math.pow(10, 1);
+          }
+          mosaicsArrayList.push(mosaics);
+        }
+        console.log(mosaicsArrayList);
+        setMosaicArray(mosaicsArrayList);
+      },
+      (err) => console.error(err)
+    );
+  };
   return (
-    <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-    </div>
+    <>
+      <IonButton onClick={getAccountMosaics}>テスト</IonButton>
+      {loop()}
+    </>
   );
 };
 
